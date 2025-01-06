4d769d57216b8f74161818e873bd6bc93c447e13
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2025-01-06 19:36:51 +0300

    feat: Api interfaces were added. .env was added

---
+++ src/types/index.ts
@@ -0,0 +1,42 @@
+// Api interfaces
+
+export interface IProduct {
+	id: string;
+	description: string;
+	image: string;
+	title: string;
+	category: TProductCategory;
+	price: number | null;
+}
+
+export interface IProductList {
+	total: number;
+	items: IProduct[];
+}
+
+export interface IOrderSuccess {
+	id: string;
+	total: number;
+}
+
+export interface IError {
+	error: string;
+}
+
+export interface IOrderRequest {
+	payment: string,
+	email: string;
+	phone: string;
+	address: string;
+	total: number;
+	items: string[];
+}
+
+// Additional interfaces
+
+export type TProductCategory =
+	| 'софт-скил'
+	| 'другое'
+	| 'дополнительное'
+	| 'кнопка'
+	| 'хард-скил';
